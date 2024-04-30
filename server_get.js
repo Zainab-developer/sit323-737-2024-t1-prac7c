606bbbcaedc0d57f55d636f6a64d14a8458bdacf
Zainab-developer <62815659+Zainab-developer@users.noreply.github.com>
2024-04-30 17:49:41 +1000

    commit

---
+++ server_get.js
@@ -0,0 +1,21 @@
+const express= require("express");
+const app= express();
+const subTwoNumber= (n1,n2) => {
+    return n1-n2;
+} //change add to sub
+app.get("/subTwoNumber", (req,res)=>{
+    const n1= parseInt(req.query.n1);
+    const n2=parseInt(req.query.n2);
+    const result = subTwoNumber(n1,n2);
+    res.json({statuscocde:200, data: result }); 
+});
+app.get("/", (req, res) => {
+    const n1 = "<html><body><H1>HELLO THERE </H1></body></html>";
+    res.set('Content-Type', 'text/html');
+    res.send(Buffer.from(n1));     
+})
+console.log (subTwoNumber(19,12));
+const port=3040;
+app.listen(port,()=> {
+    console.log("hello i'm listening to port "+port);
+})
